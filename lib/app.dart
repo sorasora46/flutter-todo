be80e32dd44924328c6b43878263ff4396b868dd
sora-k46 <sorrawit456789@gmail.com>
2023-03-06 23:42:37 +0700

    Remove template code

---
+++ lib/app.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/material.dart';
+
+class App extends StatelessWidget {
+  const App({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+        home: Scaffold(
+      appBar: AppBar(
+        title: const Text("To-Do Application"),
+      ),
+      body: const Text("Hello, World!"),
+    ));
+  }
+}
