bc0774af284e88597b3855d3276a0750a3e353b1
sora-k46 <sorrawit456789@gmail.com>
2023-03-09 00:15:46 +0700

    Create home_page.dart

---
+++ lib/pages/home_page.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+
+class HomePage extends StatelessWidget {
+  const HomePage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Center(
+      child: ElevatedButton(
+        onPressed: () {
+          Navigator.pop(context);
+        },
+        child: const Text('Go back'),
+      ),
+    );
+  }
+}
