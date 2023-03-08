2dc18dd67359d58546f4d5bf345afc4a830179bb
sora-k46 <sorrawit456789@gmail.com>
2023-03-08 21:55:49 +0700

    Add colors

---
+++ lib/constants/colors.dart
@@ -0,0 +1,6 @@
+import 'package:flutter/material.dart';
+
+const Color primaryColor = Color(0xff212737);
+const Color accentColor = Color(0xffff7562);
+const Color textColor = Color(0xff300607);
+const Color backgroundColor = Color(0xfff7fafb);
