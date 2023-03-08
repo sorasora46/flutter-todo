dc4f8976dff3dcc07d1294e607b5b72302826fcf
sora-k46 <sorrawit456789@gmail.com>
2023-03-09 00:11:18 +0700

    Convert to StatefulWidget

--- lib/pages/welcome_page.dart
+++ lib/pages/welcome_page.dart
@@ -1,9 +1,14 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_todo/constants/colors.dart';
 
-class WelcomePage extends StatelessWidget {
+class WelcomePage extends StatefulWidget {
   const WelcomePage({super.key});
 
+  @override
+  State<WelcomePage> createState() => _WelcomePageState();
+}
+
+class _WelcomePageState extends State<WelcomePage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
