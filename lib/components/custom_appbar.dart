9776fcd670613df4ab126fe6b060baaf0266fe30
sora-k46 <sorrawit456789@gmail.com>
2023-04-09 03:17:25 +0700

    Create custom_appbar.dart

---
+++ lib/components/custom_appbar.dart
@@ -0,0 +1,27 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_todo/constants/colors.dart';
+
+class CustomAppBar extends StatelessWidget implements PreferredSizeWidget {
+  const CustomAppBar({super.key})
+      : preferredSize = const Size.fromHeight(kToolbarHeight);
+
+  @override
+  final Size preferredSize;
+
+  @override
+  Widget build(BuildContext context) {
+    return AppBar(
+        backgroundColor: backgroundColor,
+        foregroundColor: textColor,
+        elevation: 0,
+        automaticallyImplyLeading: false,
+        title: Row(
+          mainAxisAlignment: MainAxisAlignment.center,
+          mainAxisSize: MainAxisSize.min,
+          children: const [
+            Icon(Icons.check_circle_outline),
+            Text('To-Do Application')
+          ],
+        ));
+  }
+}
