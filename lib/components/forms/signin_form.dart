d75ea16233c50da3569efa8b2066128fd9a3fabd
sora-k46 <sorrawit456789@gmail.com>
2023-04-17 17:42:52 +0700

    Create signin_form.dart

---
+++ lib/components/forms/signin_form.dart
@@ -0,0 +1,125 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_todo/pages/home_page.dart';
+import 'package:flutter_todo/pages/signup_page.dart';
+import 'package:flutter_todo/services/auth_service.dart';
+
+class SignInForm extends StatefulWidget {
+  const SignInForm({super.key});
+
+  @override
+  State<SignInForm> createState() => _SignInFormState();
+}
+
+class _SignInFormState extends State<SignInForm> {
+  bool _loading = false;
+  final _formKey = GlobalKey<FormState>();
+  final emailController = TextEditingController();
+  final passwordController = TextEditingController();
+
+  void handleLogin() async {
+    if (!_formKey.currentState!.validate()) {
+      ScaffoldMessenger.of(context).showSnackBar(
+          const SnackBar(content: Text('Please enter your email/password')));
+      return;
+    }
+    final String email = emailController.value.text;
+    final String password = passwordController.value.text;
+
+    setState(() {
+      _loading = true;
+    });
+
+    await AuthService().signInWithEmailAndPassword(email, password);
+
+    setState(() {
+      _loading = false;
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Form(
+      key: _formKey,
+      child: Container(
+        margin: const EdgeInsets.only(top: 50),
+        child: Column(
+          children: [
+            Container(
+              margin: const EdgeInsets.only(top: 10),
+              child: const Text('Sign in',
+                  style: TextStyle(fontSize: 20, fontWeight: FontWeight.bold)),
+            ),
+            Container(
+              margin: const EdgeInsets.only(left: 16, right: 16),
+              child: TextFormField(
+                  validator: (value) {
+                    if (null == value || value.isEmpty) {
+                      return 'Please enter your email';
+                    }
+                    return null;
+                  },
+                  controller: emailController,
+                  decoration: const InputDecoration(
+                      labelText: 'Email', hintText: 'ex. example@mail.com')),
+            ),
+            const SizedBox(
+              height: 20,
+            ),
+            Container(
+              margin: const EdgeInsets.only(left: 16, right: 16),
+              child: TextFormField(
+                  validator: (value) {
+                    if (null == value || value.isEmpty) {
+                      return 'Please enter your password';
+                    }
+                    return null;
+                  },
+                  controller: passwordController,
+                  obscureText: true,
+                  enableSuggestions: false,
+                  autocorrect: false,
+                  decoration: const InputDecoration(labelText: 'Password')),
+            ),
+            const SizedBox(
+              height: 20,
+            ),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                ElevatedButton(
+                    onPressed: () {
+                      handleLogin();
+                      Navigator.pushReplacement(
+                          context,
+                          MaterialPageRoute(
+                              builder: (context) => const HomePage()));
+                    },
+                    child: _loading
+                        ? const SizedBox(
+                            width: 20,
+                            height: 20,
+                            child: CircularProgressIndicator(
+                              color: Colors.white,
+                              strokeWidth: 2,
+                            ),
+                          )
+                        : const Text('Sign in')),
+                const SizedBox(
+                  width: 16,
+                ),
+                ElevatedButton(
+                    onPressed: () {
+                      Navigator.push(
+                          context,
+                          MaterialPageRoute(
+                              builder: (context) => const SignUpPage()));
+                    },
+                    child: const Text('Sign up'))
+              ],
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
