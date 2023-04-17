6cf43abc18a3a7423ad6e2182dee657e37a74870
sora-k46 <sorrawit456789@gmail.com>
2023-04-17 15:21:17 +0700

    Init Firebase service

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,9 @@
 import 'package:flutter/material.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'package:flutter_todo/firebase_options.dart';
 import 'app.dart';
 
-void main() {
+Future<void> main() async {
+  await Firebase.initializeApp(options: DefaultFirebaseOptions.currentPlatform);
   runApp(const App());
 }
