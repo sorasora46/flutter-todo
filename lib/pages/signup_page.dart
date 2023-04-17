a3b851fd8c921f2379c678bbc7cffd803e73d994
sora-k46 <sorrawit456789@gmail.com>
2023-04-17 18:29:06 +0700

    Render SignUpForm

--- lib/pages/signup_page.dart
+++ lib/pages/signup_page.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_todo/components/custom_appbar.dart';
+import 'package:flutter_todo/components/forms/signup_form.dart';
 
 class SignUpPage extends StatelessWidget {
   const SignUpPage({super.key});
@@ -7,8 +8,6 @@ class SignUpPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return const Scaffold(
-      appBar: CustomAppBar(),
-      body: Text('Sign up page'),
-    );
+        appBar: CustomAppBar(), body: Center(child: SignUpForm()));
   }
 }
