67ee673ac093240d186caa83f30377edefe33f1a
sora-k46 <sorrawit456789@gmail.com>
2023-04-17 17:43:16 +0700

    Remove FirebaseAuth

--- lib/main.dart
+++ lib/main.dart
@@ -1,13 +1,9 @@
-import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter_todo/firebase_options.dart';
-import 'package:flutter_todo/services/auth_service.dart';
 import 'app.dart';
 
 Future<void> main() async {
-  FirebaseApp app = await Firebase.initializeApp(
-      options: DefaultFirebaseOptions.currentPlatform);
-  AuthService(auth: FirebaseAuth.instanceFor(app: app));
+  await Firebase.initializeApp(options: DefaultFirebaseOptions.currentPlatform);
   runApp(const App());
 }
