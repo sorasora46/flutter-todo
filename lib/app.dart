b63365ee6c944967cc61f0618cb257b28a1ec00d
sora-k46 <sorrawit456789@gmail.com>
2023-04-17 17:46:01 +0700

    Serve page depend on auth state

--- lib/app.dart
+++ lib/app.dart
@@ -1,4 +1,6 @@
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_todo/pages/home_page.dart';
 import './pages/welcome_page.dart';
 import './constants/colors.dart';
 
@@ -8,14 +10,22 @@ class App extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      debugShowCheckedModeBanner: false,
-      theme: ThemeData(
-        colorScheme: ColorScheme.fromSwatch()
-            .copyWith(primary: accentColor, background: backgroundColor),
-        fontFamily: 'TiltNeon',
-      ),
-      title: "To-Do Application",
-      home: const WelcomePage(),
-    );
+        debugShowCheckedModeBanner: false,
+        theme: ThemeData(
+          colorScheme: ColorScheme.fromSwatch()
+              .copyWith(primary: accentColor, background: backgroundColor),
+          fontFamily: 'TiltNeon',
+        ),
+        title: "To-Do Application",
+        // home: const WelcomePage(),
+        home: StreamBuilder<User?>(
+          stream: FirebaseAuth.instance.authStateChanges(),
+          builder: (context, snapshot) {
+            if (snapshot.hasData) {
+              return const HomePage();
+            }
+            return const WelcomePage();
+          },
+        ));
   }
 }
