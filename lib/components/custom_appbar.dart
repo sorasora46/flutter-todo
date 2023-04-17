711b2239d818697572b4614c96da81efc30527db
sora-k46 <sorrawit456789@gmail.com>
2023-04-17 17:44:46 +0700

    Add boolean for showing leading icon

--- lib/components/custom_appbar.dart
+++ lib/components/custom_appbar.dart
@@ -2,8 +2,10 @@ import 'package:flutter/material.dart';
 import 'package:flutter_todo/constants/colors.dart';
 
 class CustomAppBar extends StatelessWidget implements PreferredSizeWidget {
-  const CustomAppBar({super.key})
+  const CustomAppBar({super.key, this.showLeading = true})
       : preferredSize = const Size.fromHeight(kToolbarHeight);
+
+  final bool showLeading;
 
   @override
   final Size preferredSize;
@@ -14,7 +16,7 @@ class CustomAppBar extends StatelessWidget implements PreferredSizeWidget {
         backgroundColor: backgroundColor,
         foregroundColor: textColor,
         elevation: 0,
-        automaticallyImplyLeading: true,
+        automaticallyImplyLeading: showLeading,
         title: Row(
           mainAxisAlignment: MainAxisAlignment.center,
           mainAxisSize: MainAxisSize.min,
